d4b3ef3ebee4b6d5d32d86cd806dab9bb00fba46
Diego Villarroel <thedjv123@gmail.com>
2023-03-01 14:54:43 -0300

    Aún falta convertir idiomas/texto UTF-8

--- src/got/GUI.java
+++ src/got/GUI.java
@@ -14,7 +14,15 @@ import java.awt.Font;
 import javax.swing.JTextField;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Scanner;
 import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JTextPane;
 
 public class GUI extends JFrame {
 
@@ -24,11 +32,16 @@ public class GUI extends JFrame {
 	private JButton btnDir;
 	private JButton btnExit;
 	private Logic logica;
+	private File selectedFile;
+	private JLabel lblPercent;
+	private JTextPane tpWords;
+	private JTextPane tpPercent;
 
 	/**
 	 * Create the frame.
 	 */
 	public GUI() {
+		logica = new Logic();
 		setTitle("Winter is Coming");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 800, 560);
@@ -36,12 +49,6 @@ public class GUI extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		btnStart = new JButton("Start");
-		btnStart.setEnabled(false);
-		btnStart.setFont(new Font("HelvLight", Font.BOLD, 20));
-		btnStart.setBounds(579, 403, 165, 35);
-		contentPane.add(btnStart);
 		
 		btnDir = new JButton("Load Directory");
 		btnDir.addActionListener(new ActionListener() {
@@ -52,14 +59,14 @@ public class GUI extends JFrame {
 			}
 		});
 		btnDir.setFont(new Font("HelvLight", Font.BOLD, 20));
-		btnDir.setBounds(75, 301, 178, 35);
+		btnDir.setBounds(76, 311, 178, 35);
 		contentPane.add(btnDir);
 		
 		txtLoadingFrom = new JTextField();
 		txtLoadingFrom.setFont(new Font("HelvLight", Font.PLAIN, 16));
 		txtLoadingFrom.setText("loading from:");
 		txtLoadingFrom.setEditable(false);
-		txtLoadingFrom.setBounds(40, 245, 255, 35);
+		txtLoadingFrom.setBounds(48, 266, 255, 35);
 		contentPane.add(txtLoadingFrom);
 		txtLoadingFrom.setColumns(10);
 		
@@ -69,11 +76,61 @@ public class GUI extends JFrame {
 				System.exit(0);
 			}
 		});
+		
+		btnStart = new JButton("Start");
+		btnStart.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				logica.readTxtFile(selectedFile);
+				setTextFields();
+			}
+		});
+		btnStart.setEnabled(false);
+		btnStart.setFont(new Font("HelvLight", Font.BOLD, 20));
+		btnStart.setBounds(568, 311, 165, 35);
+		contentPane.add(btnStart);
+		
 		btnExit.setFont(new Font("HelvLight", Font.PLAIN, 20));
-		btnExit.setBounds(579, 467, 165, 35);
+		btnExit.setBounds(611, 478, 165, 35);
 		contentPane.add(btnExit);
+		
+		JLabel lblMostUsed = new JLabel("5 most used words:");
+		lblMostUsed.setFont(new Font("HelvLight", Font.BOLD, 17));
+		lblMostUsed.setBounds(367, 129, 262, 26);
+		contentPane.add(lblMostUsed);
+		
+		lblPercent = new JLabel("percentage");
+		lblPercent.setFont(new Font("HelvLight", Font.BOLD, 17));
+		lblPercent.setBounds(639, 129, 94, 26);
+		contentPane.add(lblPercent);
+		
+		tpWords = new JTextPane();
+		tpWords.setFont(new Font("HelvLight", Font.BOLD, 21));
+		tpWords.setBounds(367, 165, 262, 136);
+		tpWords.setEditable(false);
+		contentPane.add(tpWords);
+		
+		tpPercent = new JTextPane();
+		tpPercent.setFont(new Font("HelvLight", Font.BOLD, 21));
+		tpPercent.setBounds(639, 165, 94, 136);
+		tpPercent.setEditable(false);
+		contentPane.add(tpPercent);
 	}
 	
+
+
+	protected void setTextFields() {
+		String output = "";
+		String percentOutput = "";
+		for(String word:logica.mostUsedWords()) {
+			output += word+"\n";
+			percentOutput += String.format("%.2f", logica.percentageOfUsage(word))+"%\n";
+		}
+		tpWords.setText(output);
+		tpPercent.setText(percentOutput);
+	}
+
+
+
 	private boolean openFileChooser() {
 		boolean opened = false;
 		JFileChooser fileChooser = new JFileChooser();
@@ -85,8 +142,13 @@ public class GUI extends JFrame {
 		int response = fileChooser.showOpenDialog(null); //Selecciona archivo a abrir
 		
 		if (response == JFileChooser.APPROVE_OPTION) {
-			File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
-			txtLoadingFrom.setText(fileChooser.getSelectedFile().getAbsolutePath());
+			try {
+				selectedFile = new File(fileChooser.getSelectedFile().getCanonicalPath());
+				txtLoadingFrom.setText(fileChooser.getSelectedFile().getCanonicalPath());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
 			opened = true;
 		}
 		return opened;
--- src/got/Logic.java
+++ src/got/Logic.java
@@ -1,26 +1,66 @@
 package got;
 
+import java.io.*;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Logic {
 
-	
+	private String text;
+	private String[] words;
+	private HashMap<String, Integer> mostAppearances;
 	
 	public Logic() {
-		
+		text = "";
 	}
 	
-	public void readTxtFile() {
-		
+	public void readTxtFile(File url) {
+		text="";
+		String toRead;
+		try {
+			BufferedReader inputReader = new BufferedReader(new FileReader(url));
+			while ((toRead = inputReader.readLine()) != null) {
+				text += replaceSigns(toRead);
+			}
+			inputReader.close();
+			text = text.toLowerCase();
+			words = text.split(" ");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public LinkedList<String> mostUsedWords(){
-		return null;
+		mostAppearances = new HashMap<String, Integer>(); //Creo un mappeo con las palabras como clave y su cantidad de apariciones como valor
+		for(String word: words) {
+			if(!mostAppearances.containsKey(word)) mostAppearances.put(word, 1);
+			else mostAppearances.put(word, mostAppearances.get(word)+1);
+		}
+		return mostUsedWords(mostAppearances); //Devuelvo mi lista generada en un metodo privado con las 5 palabras más usadas
 	}
 	
-	public float percentageOfUsage() {
-		return 0;
+	public float percentageOfUsage(String word) {
+		float percentage = (mostAppearances.get(word)*100)/(float)words.length;
+		System.out.print(percentage);
+		return percentage;
 	}
 	
 	
+	private String replaceSigns(String text) {
+		return text.replaceAll("[^\\w\\s]", "");
+	}
+	
+	private LinkedList<String> mostUsedWords(HashMap<String, Integer> map){ 
+		LinkedList<String> toReturn = new LinkedList<String>();
+		LinkedList<Map.Entry<String, Integer>> sorted = new LinkedList<Map.Entry<String, Integer>>(map.entrySet());
+		sorted.sort(Map.Entry.comparingByValue(Comparator.reverseOrder())); //Compara los valores y ordena la lista de mayor a menor valor
+		for (int i=0; i<5; i++) { //Devuelve los 5 valores más altos
+			toReturn.add(sorted.get(i).getKey());
+		}
+		System.out.print(toReturn);
+		return toReturn;
+	}
 }
